79dee63f7d4ef267e40cc95025bd4ae71a507355
Arkadiy Lisok <arkadiy.lisok@gmail.com>
2023-11-05 22:26:09 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-    ssr: true,
-    devtools: {enabled: true},
     app: {
         baseURL: '/forsaken-lands-landing/', // baseURL: '/<repository>/'
         buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
     },
     modules: [
         '@nuxtjs/tailwindcss',
-        '@nuxt/image',
     ],
     css: [
         '@/assets/global.scss',
